00c650825a1917ba344a6ee1348daed5043129fd
Ben Dowsett <ben.dowsett@clearscore.com>
2022-07-20 16:44:28 +0100

    Add Allrecipes list and RecipeDetailFragment

--- app/src/main/java/com/example/foodapp/RecipeDetailFragment.kt
+++ app/src/main/java/com/example/foodapp/RecipeDetailFragment.kt
@@ -5,9 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import androidx.navigation.fragment.navArgs
 import com.example.foodapp.databinding.FragmentRecipeDetailBinding
 
@@ -38,34 +35,18 @@ class RecipeDetail : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        var binding = FragmentRecipeDetailBinding.inflate(inflater, container, false)
 
+        var recipe = AllRecipeDetails.allRecipeDetails[args.recipeId.toInt() - 1]
+        binding.recipeIngredients.text = recipe.ingredients
+        binding.recipeName.text = recipe.name
+        binding.recipeImage.setImageResource(recipe.image)
+        binding.recipeInstructions.text = recipe.instructions
 
-
-        return inflater.inflate(R.layout.fragment_recipe_detail, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-
-
-        var recipe = AllRecipeDetails.allRecipeDetails[args.recipeId.toInt()-1]
-
-        //Toast.makeText(context, "${recipe}", Toast.LENGTH_LONG ).show()
-
-        var ingredients = view?.findViewById<TextView>(R.id.recipeIngredients)
-        ingredients?.text = recipe.ingredients
-
-        var recipeName = view?.findViewById<TextView>(R.id.recipeName)
-        recipeName?.text = recipe.name
-
-        var recipeImage = view?.findViewById<ImageView>(R.id.recipeImage)
-        recipeImage?.setImageResource(recipe.image)
-
-        var recipeInstructions = view?.findViewById<TextView>(R.id.recipeInstructions)
-        recipeInstructions?.text = recipe.instructions
+        return binding.root
 
     }
+
 
 
 }
